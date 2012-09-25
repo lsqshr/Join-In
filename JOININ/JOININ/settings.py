017c8b1461f27ee0cfd1ade73d7ea2fa3de9813e
Siqi <lsqshr@gmail.com>
2012-09-25 17:55:12 +1000

    changed the structure of project back

--- JOININ/JOININ/settings.py
+++ JOININ/JOININ/settings.py
@@ -16,7 +16,7 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
@@ -27,7 +27,7 @@ DATABASES = {
         'PORT': '3306', # Set to empty string for default. Not used with sqlite3.
     }
 }
-'''
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
