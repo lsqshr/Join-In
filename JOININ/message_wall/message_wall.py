da34483e6b5957324bd80c4082cc80d0a394a9b8
Siqi <lsqshr@gmail.com>
2012-09-25 16:56:41 +1000

    finished config heroku! added message wall class without implementation

---
+++ JOININ/message_wall/message_wall.py
@@ -0,0 +1,17 @@
+'''
+Created on 25/09/2012
+
+@author: siqi
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(selfparams):
+        '''
+        Constructor
+        '''
+        
