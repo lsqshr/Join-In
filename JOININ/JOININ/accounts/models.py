9bc096e527b52d5634a1ff11727bc1dbc5ec7c8a
davidfarias <davidfarias@192.168.1.112>
2012-09-30 11:25:45 +1000

    Models update

--- JOININ/JOININ/accounts/models.py
+++ JOININ/JOININ/accounts/models.py
@@ -124,45 +124,7 @@ class JoinInGroup(models.Model):
         
 #append to class declaration of JoinInUser to avoid circular dependency
 JoinInUser.groups = ManyToManyField(JoinInGroup,related_name="joinin_user_groups")
-          
-class Message(models.Model):
-    reply_to = models.ForeignKey(Message)
-    file_link = models.ForeignKey(File)
-    priority = models.IntegerField()
-    date_time = models.DateTimeField()
-    belongs_to_group = models.ForeignKey(JoinInGroup)
-    written_by = models.ForeignKey(JoinInUser)
-    content = models.CharField(1000)
 
-    def add_file_link(self, link):
-        self.file_link=link
-        return
-    
-    def set_priority(self, p):
-        self.priority=p
-        return
-    
-    def set_date_time(self):
-        self.date_time=datetime.datetime.now()
-        return
-    
-    def enter_content(self,c):
-        self.content=c
-        return
-
-class File(models.Model):
-    name = models.CharField(20)
-    last_edited = models.DateTimeField()
-    uploaded_by = models.ForeignKey(JoinInUser)
-    belongs_to_group = models.ForeignKey(JoinInGroup)
-    
-    def set_name(self,n):
-        self.name=n
-        return
-    
-    def update_time(self):
-        self.last_edited=datetime.datetime.now()
-        return
     
 class Feedback(models.Model):
     date_time = models.DateTimeField()
