4a62c73b59dcfd206564f72be2d592abe4599729
Siqi <lsqshr@gmail.com>
2012-10-12 14:22:36 +1100

    recovered the repo in 3 hours!!

--- JOININ/JOININ/message_wall/admin.py
+++ JOININ/JOININ/message_wall/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from JOININ.message_wall.models import *
 admin.site.register(Message)
 admin.site.register(PrivateMessage)
-admin.site.register(File)
+#admin.site.register(JoinInFile)
