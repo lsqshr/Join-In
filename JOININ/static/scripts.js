ace04d828bee8b97efbe82ba301bdbfe94d55c6f
hangyu che <hangyu.che@gmail.com>
2012-10-13 12:31:16 +1100

    fixed message wall page

--- JOININ/static/scripts.js
+++ JOININ/static/scripts.js
@@ -8,3 +8,29 @@ function settings() {
 		alert("You want to modify Settings!");
 }
 
+function mostraEscondeMsg(idCampo, mostra){	
+
+	if (mostra == true) {
+		document.getElementById(idCampo).style.display = "block";
+	} else {
+		document.getElementById(idCampo).style.display = "none"; 
+	}		
+}
+	
+function fechaTempo(idCampo, tempo){
+	tempo = Math.ceil(tempo);
+	setTimeout("document.getElementById('"+idCampo+"').style.visibility='hidden'", tempo);
+}
+
+function addGroups(idField, value) {
+	
+	oldValue = document.getElementById(idField).value;
+	newValue = oldValue + ";" + value;
+	document.getElementById(idField).value = newValue;
+	
+}
+
+
+
+
+
