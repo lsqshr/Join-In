b9f7c6b75d07b5da453ca05e889da1b0f98630ea
Siqi <lsqshr@gmail.com>
2012-10-19 12:53:09 +1100

    MESSAGE BAR COLOR CHANGED/ READ /UNREAD

--- JOININ/JOININ/message_wall/urls.py
+++ JOININ/JOININ/message_wall/urls.py
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
     url(r'^(?P<link>\b(accept|deny)\b)/(?P<group_id>\d+)/$', 'JOININ.message_wall.views.private_message_wall'),
     url(r'^(?P<link>view)/notification_set_read/(?P<notification_id>\d+)/$','JOININ.message_wall.views.private_message_wall'),
     url(r'^(?P<link>view)/(?P<see>\b(all_notifications|unread_notifications)\b)/$','JOININ.message_wall.views.private_message_wall'),
+    url(r'^view/(?P<mark>\b(mark_as_read|mark_as_unread)\b)/(?P<message_id>\d+)/$','JOININ.message_wall.views.mark_read'),
     url(r'^group/(?P<group_id>\d+)/(?P<link>\b(view|invite|leave)\b)/$', 'JOININ.message_wall.views.group_message_wall'),
     url(r'^group/(?P<group_id>\d+)/(?P<link>\b(accept|deny)\b)/(?P<username>\b([a-z0-9._%-]+@[a-z0-9.-]+\.[a-z]{2,4})\b)/$', 'JOININ.message_wall.views.group_message_wall'),
 )
