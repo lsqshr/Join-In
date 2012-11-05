c7bc8a23971d24f2680b2bd9dcbe6d57e5afcb53
Siqi <lsqshr@gmail.com>
2012-11-05 23:13:29 +1100

    modified git_commit.py as rm the pyc files before git rm

--- git_commit.py
+++ git_commit.py
@@ -1,5 +1,7 @@
 import os
 #clean all the pyc
+os.system("find . -name \"*.pyc\" -exec rm -f {} \;")
+os.system("find . -name \"*.swp\" -exec rm -f {} \;")
 os.system("find . -name \"*.pyc\" -exec git rm -f {} \;")
 os.system("find . -name \"*.swp\" -exec git rm -f {} \;")
 print "All the *.pyc files are cleaned"
